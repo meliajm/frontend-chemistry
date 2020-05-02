0880f8301fd603acc8e436600905f5c26cfe05f8
github username <meliajm@gmail.com>
2020-05-01 17:52:02 -0700

    added electrons to atoms in proper places

--- src/components/AtomsShow.js
+++ src/components/AtomsShow.js
@@ -11,8 +11,8 @@ export class AtomsShow extends Component {
     }
 
     electronRadius = 5
-    startingXpos = 500
-    startingYpos = 200
+    startingXpos = 300
+    startingYpos = 300
 
     componentDidMount() {
         let id = this.props.match.params.id
@@ -22,75 +22,58 @@ export class AtomsShow extends Component {
             .then(atom => this.setState({ atom }))
     }
 
-    handleDragStart = e => {
-        console.log('start drag')
-        e.target.setAttrs({
-        });
-      };
-
-    handleDragEnd = e => {
-    console.log('end drag')
-    console.log(e.target)
-    if (e.target._lastPos.x === 410 && e.target._lastPos.y === 150) {
-        console.log('you did it!')
-    }
-    
-    };
-
-    // makeElectron(i) {
-    //     return (
-    //         <Circle draggable x={500+i} y={200+i} radius={this.electronRadius} fill="black" />
-    //     )
-    // }
-
-    // renderElectrons() {
-    //     const atomicNumberInt = parseInt(this.state.atom.atomic_number)
-
-    //     for (let i=0; i<atomicNumberInt; i++){
-    //         return this.makeElectron(i)
-    //     }
-    // }
-
     renderElectrons() {
         const arr = []
-        const atomicNumberInt = (this.state.atom.atomic_number)
-        
-        for (let i=0; i<atomicNumberInt; i++){
+        let valenceElectrons = this.state.atom.atomic_number
+        // const valenceElectrons = (this.state.atom.atomic_number)
+        if (this.state.atom.atomic_number > 2) {
+            valenceElectrons = this.state.atom.atomic_number - 2
+        }
+        if (this.state.atom.atomic_number > 10) {
+            valenceElectrons = this.state.atom.atomic_number - 10
+        }
+        for (let i=0; i<valenceElectrons; i++){
             arr.push(i)
-            }
-        
-            console.log(atomicNumberInt)
+        }        
         return (
             <>
             {arr.map( i => {
-                console.log(i)
-                // console.log(circle)
-                return <Circle draggable x={500+i} y={200+i} radius={this.electronRadius} fill="black" />
+                console.log('i', i)
+                console.log('ar', parseInt(this.state.atom.atomic_radius))
+                console.log('startingXpos', this.startingXpos)
+                if (i === 0) {
+                    // return <Circle draggable x={this.startingXpos+parseInt(this.state.atom.atomic_radius)*i-2*20} y={this.startingYpos+parseInt(this.state.atom.atomic_radius)} radius={this.electronRadius} fill="black" />
+                    return <Circle draggable x={this.startingXpos-1*20} y={this.startingYpos+parseInt(this.state.atom.atomic_radius)} radius={this.electronRadius} fill="black" />                    
+                } else if ( i === 1) {
+                    return <Circle draggable x={this.startingXpos+1*20} y={this.startingYpos+parseInt(this.state.atom.atomic_radius)} radius={this.electronRadius} fill="black" />                    
+                } else if ( i === 2) {
+                    return <Circle draggable x={this.startingXpos-1*20} y={this.startingYpos-parseInt(this.state.atom.atomic_radius)} radius={this.electronRadius} fill="black" />                      
+                } else if ( i === 3) {
+                    return <Circle draggable x={this.startingXpos+1*20} y={this.startingYpos-parseInt(this.state.atom.atomic_radius)} radius={this.electronRadius} fill="black" />                    
+                } else if ( i === 4) {
+                    return <Circle draggable x={this.startingXpos-parseInt(this.state.atom.atomic_radius)} y={this.startingYpos-20} radius={this.electronRadius} fill="black" />                    
+                } else if ( i === 5) {
+                    return <Circle draggable x={this.startingXpos-parseInt(this.state.atom.atomic_radius)} y={this.startingYpos+20} radius={this.electronRadius} fill="black" />                    
+                } else if ( i === 6) {
+                    return <Circle draggable x={this.startingXpos+parseInt(this.state.atom.atomic_radius)} y={this.startingYpos+20} radius={this.electronRadius} fill="black" />                                        
+                } else if ( i === 7) {
+                    return <Circle draggable x={this.startingXpos+parseInt(this.state.atom.atomic_radius)} y={this.startingYpos-20} radius={this.electronRadius} fill="black" />                                        
+                }
             })}
             </>
         )
-
     }
     
     renderAtom() {
         
-
         return (
             <Stage width={window.innerWidth} height={window.innerHeight}>
             <Layer>
-              
               <Text x={75} y={20} text={this.state.atom.name} fontSize={15} />
-    
-              <Circle className="electronOrbit" x={500} y={200} radius={this.state.atom.atomic_radius} stroke="black" />
-
-              <Circle  className="nucleus" x={500} y={200} radius={this.state.atom.atomic_radius/10} fill={`#${this.state.atom.cpk_hex_color}`}/>
-              <Circle  className="nucleus" x={500} y={200} radius={this.state.atom.atomic_radius/10} stroke="black" strokeWidth='1'/>
-              {/* {this.renderArrayElectrons()} */}
+              <Circle className="electronOrbit" x={this.startingXpos} y={this.startingYpos} radius={this.state.atom.atomic_radius} stroke="black" />
+              <Circle  className="nucleus" x={this.startingXpos} y={this.startingYpos} radius={this.state.atom.atomic_radius/10} fill={`#${this.state.atom.cpk_hex_color}`}/>
+              <Circle  className="nucleusOutline" x={this.startingXpos} y={this.startingYpos} radius={this.state.atom.atomic_radius/10} stroke="black" strokeWidth='1'/>
               {this.renderElectrons()}
-
-              {/* <Circle draggable x={100} y={80} radius={5} fill="black" 
-              onDragStart={this.handleDragStart}
-              onDragEnd={this.handleDragEnd} /> */}
             </Layer>
           </Stage>
         )
@@ -102,8 +85,12 @@ export class AtomsShow extends Component {
                 {/* <AtomList/> */}
                 <h3>{this.state.atom.name}</h3>
                 <p>{this.state.atom.symbol}</p>
-                <p>Atomic number: {this.state.atom.atomic_number}</p>
                 <p>{this.state.atom.atomic_radius}</p>
+                <p>Atomic number: {this.state.atom.atomic_number}</p>
+                <p>How many electrons does this atom have?</p>
+                <p>How many valence electrons does this atom have?</p>
+                <p>How many protons does this atom have?</p>
+
                 <p>{this.state.atom.cpk_hex_color}</p>
                 {this.renderAtom()}
             </div>
