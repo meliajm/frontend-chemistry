15551c64bbc8932df6a91648b1172ffcfb4a8bc2
github username <meliajm@gmail.com>
2020-05-13 15:05:07 -0700

    add question container

---
+++ src/actions/questionAction.js
@@ -0,0 +1,3 @@
+// add question
+// delete question
+// update question
---
+++ src/containers/QuestionsContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchUsers } from '../actions/userActions'
+
+
+export class QuestionsContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default QuestionsContainer
