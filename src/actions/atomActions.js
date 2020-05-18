b955f99cffd6c261d2365773c2a226f71329bd2b
github username <meliajm@gmail.com>
2020-05-17 20:23:28 -0700

    back to localhost 3001

--- src/actions/atomActions.js
+++ src/actions/atomActions.js
@@ -1,5 +1,5 @@
-// const baseURL = "http://localhost:3001/api/v1/"
-const baseURL = "https://warm-fortress-71971.herokuapp.com/api/v1/"
+const baseURL = "http://localhost:3001/api/v1/"
+// const baseURL = "https://warm-fortress-71971.herokuapp.com/api/v1/"
 
 export const fetchAtoms = () => {
     return (dispatch) => {
